360ba5ed34b6174c228a264deb1becaf4f6774be
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2019-08-06 14:36:55 -0300

    Comentarios Adicionais

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = express.Router();
+const routes = express.Router(); // Express especial somente para as rotas
 const DevController = require('./controllers/DevController');
 const LikeController = require('./controllers/LikeController');
 const DislikeController = require('./controllers/DislikeController');
@@ -13,10 +13,10 @@ routes.get('/', (request, response) => {
 });
 
 // Cria as Rotas
-routes.get('/devs', DevController.index);
-routes.post('/devs', DevController.store);
-routes.post('/devs/:devId/likes', LikeController.store);
-routes.post('/devs/:devId/dislikes', DislikeController.store);
+routes.get('/devs', DevController.index); // Pede a lista de usuarios
+routes.post('/devs', DevController.store); // Cadastra novo usuario usando json 
+routes.post('/devs/:devId/likes', LikeController.store); // Da like em um usuario
+routes.post('/devs/:devId/dislikes', DislikeController.store); // Da deslike em um usuario
 
 // Deixa essa variavel exposta para ser consultada por qualquer ums
 module.exports = routes;
